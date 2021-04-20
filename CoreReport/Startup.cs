51034a69a36fa99bb191ef223591662ca8b5a66e
A.K.M Maruful Islam <59158377+marufulDev@users.noreply.github.com>
2021-04-21 00:01:04 +0600

    datepicker add

--- CoreReport/Startup.cs
+++ CoreReport/Startup.cs
@@ -58,7 +58,7 @@ namespace CoreReport
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Reporting}/{action=Index}/{id?}");
             });
         }
     }
